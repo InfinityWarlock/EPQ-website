f963b2d6b76693ba0b67f78861cf9a5fa5500117
InfinityWarlock <theo.hintonhallows123@gmail.com>
2020-01-27 11:20:33 +0000

    get coords to display postcode on map which works

--- tradingwebsite/static/scripts/map.js
+++ tradingwebsite/static/scripts/map.js
@@ -1,7 +1,7 @@
 APIToken = 'pk.eyJ1IjoiY2F0bmlwbWFzdGVycmFjZSIsImEiOiJjazVuemY3Y2sxYm9mM21xazRtNW9wMGY4In0.DNyNPfASNJAKCUBArIZkSg';
 mapboxgl.accessToken = APIToken;
 
-console.log("found js file");
+console.log("code is updated");
 
 function httpGetAsync(theUrl, callback){
     var xmlHttp = new XMLHttpRequest();
@@ -14,11 +14,14 @@ function httpGetAsync(theUrl, callback){
 }
 
 function createMap(postcode, map_id){
-    var map = new mapboxgl.Map({
-        container: map_id,
-        style: 'mapbox://styles/mapbox/streets-v11'
+    
+    var postcodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+encodeURI(postcode)+".json?access_token="+encodeURI(APIToken);
+    console.log(postcodeURL)
+    httpGetAsync(postcodeURL, response => {
+        var lnglat = JSON.parse(response).features[0].center;
+        var map = new mapboxgl.Map({
+            container: map_id,
+            style: 'mapbox://styles/mapbox/streets-v11'
+        });
     });
-    var postcodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+encodeURI(postcode)+".json";
-    var lnglat = httpGetAsync(postcodeURL, function(response){return response});
-    console.log(lnglat)
 }
