d8641ef62fe9ab1252923b63293cfb01c5034c21
InfinityWarlock <theo.hintonhallows123@gmail.com>
2020-03-01 23:45:09 +0000

    mongodb env var

--- tradingwebsite/__init__.py
+++ tradingwebsite/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'd214895cc0f65074680c2468af85c90d'
 
-db_connection_string = "mongodb+srv://CatnipMasterRace:XuUrP6Ui5EErAcro@epq-trading-site-q3dcn.gcp.mongodb.net/test?retryWrites=true&w=majority"
+db_connection_string = MONGODB_URI
 client = pymongo.MongoClient(db_connection_string)
 db = client.epq_trading_site
 
